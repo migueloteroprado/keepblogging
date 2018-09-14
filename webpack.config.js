a57eba23af695961824bfa8c1d3f9f2bbb3b648f
Miguel <miguelitri@gmail.com>
2018-09-14 10:17:49 +0200

    Add babel polyfill. Add ejs and partials

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,10 @@
 var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var CleanWebpackPlugin = require('clean-webpack-plugin');
+var Dotenv = require('dotenv-webpack');
 
 module.exports = {
-	entry: path.join(__dirname, 'src', 'index'),
+	entry: ['babel-polyfill', path.join(__dirname, 'src', 'index')],
 	output: {
 		filename: 'bundle[hash].js',
 		path: path.resolve(__dirname, 'dist')
@@ -16,6 +17,7 @@ module.exports = {
 		port: 3000
 	},
 	plugins: [
+		new Dotenv(),
 		new HtmlWebpackPlugin({
 			title: 'Keep Blogging',
 			template: path.join(__dirname, 'src', 'pages', 'index', 'index.html'),
@@ -26,9 +28,32 @@ module.exports = {
 		new CleanWebpackPlugin(['dist'])
 	],
 	module: {
-		rules: [{
-			test: /\.css$/,
-			use: ['style-loader', 'css-loader']
-		}]
-	}
+		rules: [
+			{
+				test: /\.(js)$/,
+				exclude: /node_modules/,
+				use: "babel-loader"
+			},
+			{
+				test: /\.css$/,
+				use: ['style-loader', 'css-loader']
+			},
+			{
+				test: /\.(html|ejs)$/,
+				use: ['html-loader', 'ejs-html-loader']
+			}
+		]
+	},
+	resolve: {
+	  alias: {
+		components: path.resolve(__dirname, 'src', 'components'),
+		assets: path.resolve(__dirname, 'src', 'assets'),
+		styles: path.resolve(__dirname, 'src', 'styles'),
+		utils: path.resolve(__dirname, 'src', 'utils'),
+		data: path.resolve(__dirname, 'src', 'data'),
+		services: path.resolve(__dirname, 'src', 'services'),
+		pages: path.resolve(__dirname, 'src', 'pages')
+	  }
+	},
+	devtool: 'source-map'
 };
