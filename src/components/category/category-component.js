84c9557365d1dccf88f7dbd855ac65c20600728c
Miguel Angel Otero <miguelitri@gmail.com>
2018-09-21 03:35:25 +0200

    Categories page styles. Resize assets.

--- src/components/category/category-component.js
+++ src/components/category/category-component.js
@@ -1,11 +1,7 @@
 export const createCategory = ({ id,	name } = { id: 0, name: '' }) => {
 	const category = document.createElement('article');
 	category.classList.add('category');
-	category.innerHTML = `
-		<header class="category-header">
-			<a class="category-title" href="/?category=${id}">${name}</a>
-		</header>
-	`;
+	category.innerHTML = `<a class="category-title" href="/?category=${id}">${name}</a>`;
 
 	return category;
 };
